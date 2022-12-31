e45459b8643b24966a1b200ae83579966dd221e4
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2022-12-31 13:38:16 +0100

    contract issues fixed

--- src/OracleV1.source.ts
+++ src/OracleV1.source.ts
@@ -14,7 +14,7 @@ export const oracleMasterSourceV1 = () => {
   }
 
 const oracleMasterSourceV1CodeBoc =
-  'te6ccsEBBgEAywANEiEmecsBFP8A9KQT9LzyyAsBAgFiAwIAGaC149qJofSBqan0gGECAswFBACh38FAgigagqmTgqEgGqEigaOGRln6gB54sA54tmAOeLZJDkZYKJ+gAJegBlgGSQfIA4OmRlgWUD5f/k6DvADGRlgqxniygB/QEJZbXmZLj9gEAJ/ZmRY4BJL4HwaGmB/SAYALjYSS+B8BDBBJiWgFz5cCf2omh9IGpqfSAYNhECaY/pn5j9IBiCQQSYloBQgOAAzIH9IBgiAYF4BHAvgsIH+XhElSkTo='
+  'te6ccsECCAEAASAAAA0AEgAXAF8AbgBzANEBIAEU/wD0pBP0vPLICwECAWIFAgIBIAQDAIu85NdqJofSBqan0gGDYRfBQqkBk4KhIBqhIoGjhkZZ+oAeeLAOeLZgDni2SQ5GWCifoACXoAZYBk/IA4OmRlgWUD5f/k6EABm+149qJofSBqan0gGEAgLMBwYAt7XwUCCKBqCqZOCoSAaoSKBo4ZGWfqAHniwDni2YA54tkkORlgon6AAl6AGWAZJB8gDg6ZGWBZQPl/+ToAblkZY/ln+S7wAxkZYKoAueLKAH9AQnltYlmZmS4/YBAJnZmRY4BJL4HwaGmB/SAYALjYSS+B8BDBBJiWgFz5aCf2omh9IGpqfSAYNhECaY/pn/0gGALBBJiWgFCBYADLgogaIJh4BXAvg0IH+XhDtwRQo='
 
 export const oracleMasterSourceV1CodeCell = Cell.fromBoc(Buffer.from(oracleMasterSourceV1CodeBoc, 'base64'))[0];
 
@@ -31,6 +31,6 @@ export const oracleClientSourceV1 = () => {
   }
 
 const oracleClientSourceV1CodeBoc =
-  'te6ccsEBBAEALgANEiIuART/APSkE/S88sgLAQIBYgMCABug9gXaiaH0AfSB9IGoYQAU0GwxxwDchA/y8MPYmGc='
+  'te6ccsECBgEAAQ4AAA0AEgAdAJwAzAEOART/APSkE/S88sgLAQIBYgMCABGhLP/aiaGmfmEC9tAgxwCRW+AB0NMDMfpAMAHTHzDtRNDTAPpA1NT6QNQB0NcLPwHUAdD0BQH6QDAzNDQh0GwS0wcwghCYJTV4UmC6jikxMjP4J28iMIIJMS0AvI4WUTKBAQj0Cm+hMTIgwAExsZPAATDgMOBfBOAyM4IQguljQ1JAuuMCWwUEAFyCEG0tO0USuo4eIfkBAfkBuo4TcIAYyMsFWM8WIfoCy2rJgwb7AOAw4FuED/LwAIAz+CdvIjCCCTEtALwk+QED+QETuhKwjiUBwAAwcMjLH8s/yXFwgBjIywVQBM8WI/oCE8tpEssAzMmAQPsA4F8DRMa9vQ=='
 
 export const oracleClientSourceV1CodeCell = Cell.fromBoc(Buffer.from(oracleClientSourceV1CodeBoc, 'base64'))[0]
