088371cfeb9f8e7cb64378414fd569c222d5b15b
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2022-12-31 16:04:00 +0100

    first two test has been fixed

--- src/OracleV1.spec.ts
+++ src/OracleV1.spec.ts
@@ -1,165 +1,158 @@
-import BN from "bn.js";
+import BN from 'bn.js';
 import {
-  Address,
-  CellMessage,
-  fromNano,
-  beginCell,
-  Cell,
-  toNano,
-  CommonMessageInfo,
-  ExternalMessage,
-  TupleSlice4,
-} from "ton";
-import { actionToMessage } from "./utils/actionToMessage";
-import { internalMessage } from "./utils/internalMessage";
-import {
-  parseOracleDetails,
-  parseJettonWalletDetails,
-} from "./utils/jetton-utils";
-import { randomAddress, zeroAddress } from "./utils/randomAddress";
-import { OracleV1LocalMaster } from "./OracleV1LocalMaster";
-import { OracleV1LocalClient } from "./OracleV1LocalClient";
-import { convertFromExecutorStack } from "./utils/convertFromExecutorStack";
-import { OPS } from "./OracleV1.data";
-import { randomInt } from "crypto";
-import { kill } from "process";
-import { info } from "console";
+    Address,
+    CellMessage,
+    fromNano,
+    beginCell,
+    Cell,
+    toNano,
+    CommonMessageInfo,
+    ExternalMessage,
+    TupleSlice4,
+} from 'ton';
+import { actionToMessage } from './utils/actionToMessage';
+import { internalMessage } from './utils/internalMessage';
+import { parseOracleDetails, parseJettonWalletDetails } from './utils/jetton-utils';
+import { randomAddress, zeroAddress } from './utils/randomAddress';
+import { OracleV1LocalMaster } from './OracleV1LocalMaster';
+import { OracleV1LocalClient } from './OracleV1LocalClient';
+import { convertFromExecutorStack } from './utils/convertFromExecutorStack';
+import { OPS } from './OracleV1.data';
+import { randomInt } from 'crypto';
+import { kill } from 'process';
+import { info } from 'console';
 
 function assertNotNull(a: unknown): asserts a {
-  expect(a).not.toBeNull();
+    expect(a).not.toBeNull();
 }
 
 function assert(a: unknown): asserts a {
-  expect(a).toBeTruthy();
+    expect(a).toBeTruthy();
 }
 
 function assertAddress(a: unknown, b: Address) {
-  expect(a).toBeInstanceOf(Address);
-  if (a instanceof Address) {
-    expect(a.equals(b)).toBeTruthy();
-  }
+    expect(a).toBeInstanceOf(Address);
+    if (a instanceof Address) {
+        expect(a.equals(b)).toBeTruthy();
+    }
 }
 
 function assertCoins(a: BN, b: BN) {
-  expect(a.eq(b)).toBeTruthy();
+    expect(a.eq(b)).toBeTruthy();
 }
 
-const END_USER = randomAddress("end_user");
+const END_USER = randomAddress('end_user');
 
 const config = {
-  metadata: {
-    name: "USDT/TON Price Oracle",
-    image: "https://www.linkpicture.com/q/download_183.png", // Image url
-    description: "This is master oracle for USDT/TON price",
-  },
+    metadata: {
+        name: 'USDT/TON Price Oracle',
+        image: 'https://www.linkpicture.com/q/download_183.png', // Image url
+        description: 'This is master oracle for USDT/TON price',
+    },
 };
 
-describe("Oracle v1 Master", () => {
-  let masterContract: OracleV1LocalMaster;
-  let clientContract: OracleV1LocalClient;
+describe('Oracle v1 Master', () => {
+    let masterContract: OracleV1LocalMaster;
+    let clientContract: OracleV1LocalClient;
 
-  const getClientContract = async (
-    clientOwnerAddress: Address,
-    oracleMasterAddress: Address
-  ) =>
-    await OracleV1LocalClient.create(clientOwnerAddress, oracleMasterAddress);
+    const getClientContract = async (clientOwnerAddress: Address, oracleMasterAddress: Address) =>
+        await OracleV1LocalClient.create(clientOwnerAddress, oracleMasterAddress);
 
-  beforeEach(async () => {
-    masterContract = await OracleV1LocalMaster.createFromConfig(config);
-  });
+    beforeEach(async () => {
+        masterContract = await OracleV1LocalMaster.createFromConfig(config);
+    });
 
-  it("should get oracle master initialization data correctly", async () => {
-    const call = await masterContract.contract.invokeGetMethod(
-      "get_oracle_data",
-      []
-    );
+    it('should get oracle master initialization data correctly', async () => {
+        const call = await masterContract.contract.invokeGetMethod('get_oracle_data', []);
 
-    const { metadata, address } = parseOracleDetails(call);
+        const { metadata, address } = parseOracleDetails(call);
 
-    expect(address).toBeDefined();
-    expect(metadata.name).toEqual("USDT/TON Price Oracle");
-    expect(metadata.description).toEqual(
-      "This is master oracle for USDT/TON price"
-    );
-  });
+        expect(address).toBeDefined();
+        expect(metadata.name).toEqual('USDT/TON Price Oracle');
+        expect(metadata.description).toEqual('This is master oracle for USDT/TON price');
+    });
 
-  it("should send signup command and verify the outgoing data", async () => {
-    const sc_address = randomAddress("smart_contract");
+    it('should send signup command and verify the outgoing data', async () => {
+        const sc_address = randomAddress('smart_contract');
 
-    const stack = await masterContract.contract.sendInternalMessage(
-      internalMessage({
-        from: END_USER,
-        value: toNano(2),
-        body: OracleV1LocalMaster.createSignupPayload(sc_address),
-      })
-    );
+        const stack = await masterContract.contract.sendInternalMessage(
+            internalMessage({
+                from: END_USER,
+                value: toNano(2),
+                body: OracleV1LocalMaster.createSignupPayload(sc_address),
+            }),
+        );
 
-    expect(stack.exit_code).toEqual(0);
-    expect(stack.actionList.length).toEqual(1);
+        expect(stack.exit_code).toEqual(0);
+        expect(stack.actionList.length).toEqual(1);
 
-    const accountCreationMessage = stack.actionList[0];
+        const accountCreationMessage = stack.actionList[0];
 
-    // TODO: compare the account creation message destination with the calculated address of oracle client
-    const call = await masterContract.contract.invokeGetMethod(
-      "get_oracle_client_address",
-      []
-    );
-    // console.log(accountCreationMessage);
-  });
+        // TODO: compare the account creation message destination with the calculated address of oracle client
+        const call = await masterContract.contract.invokeGetMethod('get_oracle_client_address', []);
+        // console.log(accountCreationMessage);
+    });
 });
 
-describe("Oracle v1 Client", () => {
-  let clientContract: OracleV1LocalClient;
-  const tonPrice = "2.44";
+describe('Oracle v1 Client', () => {
+    let clientContract: OracleV1LocalClient;
+    const tonPrice = '2.44';
+    const senderAddress = randomAddress('notowner');
 
-  beforeEach(async () => {
-    clientContract = await OracleV1LocalClient.create();
-  });
+    beforeEach(async () => {
+        clientContract = await OracleV1LocalClient.create(null, null, senderAddress);
+    });
 
-  it("should update data on client contract correctly", async () => {
-    await clientContract.contract.sendInternalMessage(
-      internalMessage({
-        from: randomAddress("notowner"),
-        body: beginCell()
-          .storeUint(OPS.Update, 32)
-          .storeUint(toNano(tonPrice), 64)
-          .endCell(),
-      })
-    );
+    it('should update data on client contract correctly', async () => {
+        await clientContract.contract.sendInternalMessage(
+            internalMessage({
+                from: senderAddress,
+                body: beginCell().storeUint(OPS.Update, 32).storeUint(toNano(tonPrice), 64).endCell(),
+            }),
+        );
 
-    const getMethodResult = await clientContract.contract.invokeGetMethod(
-      "get_stored_coin_price",
-      []
-    );
+        const getMethodResult = await clientContract.contract.invokeGetMethod('get_stored_coin_price', []);
 
-    expect(getMethodResult.result[0]).toEqual(toNano(tonPrice));
-  });
+        expect(getMethodResult.result[0]).toEqual(toNano(tonPrice));
+    });
 
-  it("should fetch data from client contract correctly", async () => {
-    await clientContract.contract.sendInternalMessage(
-      internalMessage({
-        from: randomAddress("notowner"),
-        body: beginCell()
-          .storeUint(OPS.Update, 32)
-          .storeUint(toNano(tonPrice), 64)
-          .endCell(),
-      })
-    );
+    it('should fetch data from client contract correctly', async () => {
+        await clientContract.contract.sendInternalMessage(
+            internalMessage({
+                from: senderAddress,
+                body: beginCell().storeUint(OPS.Update, 32).storeUint(toNano(tonPrice), 64).endCell(),
+            }),
+        );
 
-    const fetchMethodResult = await clientContract.contract.sendInternalMessage(
-      internalMessage({
-        from: randomAddress("notowner"),
-        body: beginCell().storeUint(OPS.Fetch, 32).endCell(),
-      })
-    );
+        const fetchMethodResult = await clientContract.contract.sendInternalMessage(
+            internalMessage({
+                from: senderAddress,
+                body: beginCell().storeUint(OPS.Fetch, 32).endCell(),
+            }),
+        );
 
-    const action = fetchMethodResult.actionList[0] as any; // TODO: need to take type from ton lib
-    const resultValue = fromNano(
-      parseInt(
-        "0x" + String(action.message.body).slice(18).slice(0, -2).toLowerCase()
-      )
-    ); // TODO: find another way to get data from response
+        console.log(fetchMethodResult.debugLogs)
+        const action = fetchMethodResult.actionList[0] as any; // TODO: need to take type from ton lib
+        const resultValue = fromNano(
+            parseInt('0x' + String(action.message.body).slice(18).slice(0, -2).toLowerCase()),
+        ); // TODO: find another way to get data from response
 
-    expect(resultValue).toEqual(tonPrice);
-  });
+        expect(resultValue).toEqual(tonPrice);
+    });
+
+    it('should withdrawal all TONs from client contract correctly', async () => {
+        const withdrawalMethodResult = await clientContract.contract.sendInternalMessage(
+            internalMessage({
+                from: randomAddress('notowner'),
+                body: beginCell().storeUint(OPS.Withdrawal, 32).endCell(),
+            }),
+        );
+
+        const action = withdrawalMethodResult.actionList[0] as any; // TODO: need to take type from ton lib
+        const resultValue = fromNano(
+            parseInt('0x' + String(action.message.body).slice(18).slice(0, -2).toLowerCase()),
+        ); // TODO: find another way to get data from response
+
+        expect(resultValue).toEqual(tonPrice);
+    });
 });
